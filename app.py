79536d6ede271b5da806a696556808c1c533ce51
fatn28 <f.atn@outlook.com>
2026-07-07 10:59:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from AZ-104 Projekt 5 - STAGING Version!'
+    return 'Hello from AZ-104 Projekt 5 - App Service Deployment & Scaling!'
 
 
 if __name__ == '__main__':
